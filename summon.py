cdebc2d0894bf9cc8b87eb2a7b5393820a6f5fb1
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-08-29 20:39:24 +0530

    Create summon.py

---
+++ summon.py
@@ -0,0 +1,15 @@
+from pyrogram import Client 
+from config import SESSION_NAME, API_ID, API_HASH
+
+temp = Client(SESSION_NAME, API_ID, API_HASH)
+
+#async def joinchatto(chat_id):
+#  async def (client: Client):
+#    await client.join_chat(chat_id)
+#    return True
+  
+def temo(): 
+  temp.start()
+  
+def teme(): 
+  temp.stop()
