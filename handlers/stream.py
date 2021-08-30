0186391d298ea749eb8ab898fe1128f8e471ba84
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-08-30 20:25:14 +0530

    Update stream.py

--- handlers/stream.py
+++ handlers/stream.py
@@ -13,9 +13,11 @@ from helpers.decorators import authorized_users_only
 from helpers.decorators import authorized_users_only2
 from config import API_ID, API_HASH, BOT_TOKEN, PLAY_PIC, BOT_USERNAME, OWNER_ID, UBOT_ID
 import moviepy.editor as soundex
+import asyncio
+import os
+import subprocess
 
 @Client.on_message(filters.command(["stream", f"stream@{BOT_USERNAME}"]) & other_filters)
-@errors
 @authorized_users_only
 async def stream_vid(client: Client, message: Message):
   video = (message.reply_to_message.video or message.reply_to_message.document) if message.reply_to_message else None
@@ -23,10 +25,16 @@ async def stream_vid(client: Client, message: Message):
     return await message.reply_text('Reply to a file or a video....')
   if not (video.file_name.endswith('.mkv') or video.file_name.endswith('.mp4')):
     return await message.reply_text('Not a valid format...')
-  dl = await message.reply_to_message.download()
-  audio_file_name = str(dl).split('.', 1)[0]
-  ext_s = soundex.VideoFileClip(dl)
-  sound_clip = converter.convert(ext_s.audio.write_audiofile(f'{audio_file_name}.mp3'))
+  m = await message.reply_text('Downloading....')
+  file_name = f'{video.file_unique_id}.{video.file_name.split(".", 1)[-1]}'
+  dl = await message.reply_to_message.download(file_name)
+  audio_file_name = str(dl).split('.', 1)[0].replace(' ', '_') + '.wav'
+  await m.edit('Processing audio....')
+  proc = await asyncio.create_subprocess_shell(f"ffmpeg -i {str(dl)} -ab 160k -ac 2 -ar 44100 -vn {audio_file_name}",asyncio.subprocess.PIPE,stderr=asyncio.subprocess.PIPE)
+  await proc.communicate()
+  #cli = soundex.VideoFileClip(dl)
+  #cli.audio.write_audiofile(audio_file_name)
+  sound_clip = await converter.convert(audio_file_name)
   try:
       group_call = await mp.call(message.chat.id)
   except RuntimeError:
@@ -35,6 +43,8 @@ async def stream_vid(client: Client, message: Message):
       return await message.reply_text('Seems like my assistant is not in the chat!')
   except Exception as e:
       return await message.reply_text(f'{type(e).__name__}: {e}')
-  group_call.input_filename = sound_clip
   await group_call.set_video_capture(dl)
+  group_call.input_filename = sound_clip
+  await m.delete()
+  await message.reply_text(f'Streaming {video.file_name}...')
   block_chat.append(message.chat.id)
